3ee401460c849407d4f2b67875e980e0071c6c80
Juan David London <juandavid1237890@gmail.com>
2024-04-02 15:34:39 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -30,7 +30,10 @@ class ComunaController extends Controller
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.new' , ['municipios' => $municipios]);
     }
 
     /**
@@ -41,7 +44,16 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio' , 'tb_comuna.muni_codi' , '=' , 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*' , "tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     /**
@@ -63,7 +75,11 @@ class ComunaController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.edit' , ['comuna'  => $comuna, 'municipios' => $municipios]);
     }
 
     /**
@@ -75,7 +91,17 @@ class ComunaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio' , 'tb_comuna.muni_codi' , '=' , 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*' , "tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     /**
@@ -86,6 +112,13 @@ class ComunaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio' , 'tb_comuna.muni_codi' , '=' , 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*' , "tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 }
--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -13,6 +13,7 @@
   <body>
     <div class="container">
     <h1>Listado de Comunas</h1>
+    <a href="{{ route ('comunas.create')}}" class="btn btn-success">Add</a>
     <table class="table">
     <thead>
         <tr>
@@ -28,7 +29,16 @@
       <th scope="row">{{$comuna->comu_codi}}</th>
       <td>{{$comuna->comu_nomb}}</td>
       <td>{{$comuna->muni_nomb}}</td>
-      <td><span> Acciones </span></td>
+      <td>
+      <a href="{{route('comunas.edit',['comuna'=>$comuna->comu_codi])}}"
+                    class="btn btn-info"> Edit </a></li>
+                <form action="{{ route('comunas.destroy' , ['comuna' => $comuna->comu_codi]) }}"
+                    method='POST' style="display: inline-block">
+                    @method('delete')
+                    @csrf
+                    <input class="btn btn-danger" type="submit" value="Delete">
+                </form>
+      </td>
     </tr>
     @endforeach
   </tbody>
