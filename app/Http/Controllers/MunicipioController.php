3f7f41c871ca54726f848e85574e57e795b3dfac
Juan David London <juandavid1237890@gmail.com>
2024-04-02 16:28:41 -0500

    Municipio add

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Municipio;
 
 class MunicipioController extends Controller
 {
@@ -13,7 +15,11 @@ class MunicipioController extends Controller
      */
     public function index()
     {
-        $municipios = Municipio::all();
+        //$municipios = Municipio::all();
+        $municipios = DB::table('tb_municipio')
+           ->join('tb_departamento', 'tb_municipio.depa_codi', '=' , 'tb_departamento.depa_codi')
+           ->select('tb_municipio.*' , "tb_departamento.depa_nomb")
+           ->get();
         return view('municipio.index', ['municipios' => $municipios]);
     }
 
@@ -24,7 +30,10 @@ class MunicipioController extends Controller
      */
     public function create()
     {
-        //
+        $departamentos = DB::table('tb_departamento')
+        ->orderBy('depa_nomb')
+        ->get();
+    return view('municipio.new', ['departamentos' => $departamentos]);
     }
 
     /**
@@ -35,7 +44,18 @@ class MunicipioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $municipio = new Municipio();
+
+        $municipio->muni_nomb = $request->name;
+        $municipio->depa_codi = $request->code;
+        $municipio->save();
+
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=' , 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*' , "tb_departamento.depa_nomb")
+        ->get();
+
+        return view('municipio.index', ['municipios' => $municipios]);
     }
 
     /**
