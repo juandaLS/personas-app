6b149e71fb72e9d545a11b543d225aab21d66857
Juan David London <juandavid1237890@gmail.com>
2024-04-02 19:38:42 -0500

    Ultimas correcciones y finalizacion

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pais;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PaisController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\PaisController;
 use Illuminate\Support\Facades\Route;
 
 /*
